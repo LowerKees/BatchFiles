304f97bfa2f198599f38edd9dec2edde64fb5808
LowerKees <Martijn.Beenker@gmail.com>
2017-11-14 08:51:30 +0100

    Toevoegen van batch files

---
+++ NetStart_SqlServer.bat
@@ -0,0 +1,4 @@
+net start mssqlserver
+net start sqlserveragent
+net start "SQL SERVER (I_DEV)"
+net start MSSQLServerOLAPService
---
+++ NetStop_SqlServer.bat
@@ -0,0 +1,4 @@
+net stop mssqlserver
+net stop sqlserveragent
+net stop "SQL SERVER (I_DEV)"
+net stop MSSQLServerOLAPService
